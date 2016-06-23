be31d94dce1b0f4439cd04a004236bb6c53d1e45
evesgf <evesgf@163.com>
2016-06-23 19:22:36 +0800

    设计文档修改，新增SimpleJSON

---
+++ Assets/LarkFramework/Modules/Helper/Uil.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class Uil : MonoBehaviour {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static Uri AppContentDataUri
+    {
+        get
+        {
+            string dataPath = Application.dataPath;
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                return new UriBuilder
+                {
+                    Scheme = "file",
+                    Path = Path.Combine(dataPath, "Raw/")
+                }.Uri;
+            }
+
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                return new Uri("jar:file//" + dataPath + "!/assets/");
+            }
+
+            return new UriBuilder
+            {
+                Scheme = "file",
+                Path = Path.Combine(dataPath, "StreamingAssets/")
+            }.Uri;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static Uri AppPersistentDataUri
+    {
+        get
+        {
+            return new UriBuilder
+            {
+                Scheme = "file",
+                Path = Application.persistentDataPath + "/"
+            }.Uri;
+        }
+    }
+
+    /// <summary>
+    /// 安全销毁GameObject
+    /// </summary>
+    /// <param name="go"></param>
+    public static void SafeDestroy(GameObject go)
+    {
+        if (go != null)
+        {
+            UnityEngine.Object.Destroy(go);
+        }
+    }
+
+    /// <summary>
+    /// 安全销毁Transform
+    /// </summary>
+    /// <param name="go"></param>
+    public static void SafeDestroy(Transform go)
+    {
+        if (go != null)
+        {
+            Uil.SafeDestroy(go.gameObject);
+        }
+    }
+
+    //清除GC
+    public static void ClearMemory()
+    {
+        GC.Collect();
+        Resources.UnloadUnusedAssets();
+    }
+}
