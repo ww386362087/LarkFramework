1bc038b7bb55a1198448f347adea00026d1e2240
evesgf <evesgf@163.com>
2016-07-11 01:25:45 +0800

    添加Test

---
+++ Assets/LarkFramework/Modules/Test/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace LarkFramework.Test
+{
+    [RequireComponent(typeof(GameRoot))]
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        private static T _instance;
+
+        public static T GetInstance()
+        {
+            return _instance;
+        }
+
+        public void SetInstance(T t)
+        {
+            if (_instance == null)
+            {
+                _instance = t;
+            }
+        }
+
+        public virtual void Init()
+        {
+            return;
+        }
+
+        public virtual void Release()
+        {
+            return;
+        }
+    }
+}
