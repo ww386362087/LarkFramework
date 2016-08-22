b74f37286180291f6ea0fc46a948d4a892c73494
evesgf <evesgf@163.com>
2016-08-22 19:31:41 +0800

    修改升力模型

--- Assets/LarkFramework/Examples/AirPlaneLab/Scripts/LifePowerTest.cs
+++ Assets/LarkFramework/Examples/AirPlaneLab/Scripts/LifePowerTest.cs
@@ -4,21 +4,42 @@ using System.Collections;
 public class LifePowerTest : MonoBehaviour {
 
     public Transform[] windPos;
+    public float selfSpeed;
+
+    public Transform rudder;
+
+    public float maxRudderAngle;
+
+    public float wingCI;
+    public float wingA;
+
+    public float rudderCI;
+    public float rudderA;
+
+    private Quaternion quate;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        quate = rudder.transform.localRotation;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         DrawWind();
+        RudderAni();
+        CheckAngle();
+    }
 
+    private void CheckAngle()
+    {
+        //获取当前速度分量
+
+        //获取当前
     }
 
     private void RudderAni()
     {
-
+        rudder.transform.localRotation = Quaternion.Euler(quate.x + maxRudderAngle* Input.GetAxis("AirPitch"), quate.y, quate.z);
     }
 
     private void DrawWind()
