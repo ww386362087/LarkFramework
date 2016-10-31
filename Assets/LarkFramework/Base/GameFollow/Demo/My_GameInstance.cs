0be38b253741b11b0a65f780fd20ae1cac50dabe
evesgf <evesgf@163.com>
2016-10-31 23:27:36 +0800

    GameFollow的Base层添加虚函数限定，同时添加使用Demo

---
+++ Assets/LarkFramework/Base/GameFollow/Demo/My_GameInstance.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using LarkFramework.GameFollow;
+
+public class My_GameInstance : GameInstanceBase<My_GameInstance>
+{
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        My_GameInstance.Create();
+        My_GameMode.Create().Init(this);
+    }
+
+    void Update()
+    {
+        My_GameMode.Instance.OnUpdate();
+    }
+
+    void OnDestroy()
+    {
+        My_GameMode.Instance.OnDestroy();
+    }
+
+    void OnApplicationQuit()
+    {
+
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+
+    }
+}
---
+++ Assets/LarkFramework/Base/GameFollow/Demo/My_GameMode.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using LarkFramework.GameFollow;
+
+public class My_GameMode : GameModeBase<My_GameMode>
+{
+    public My_GameInstance gameInstance { get; private set; }
+    public GameObject gameInstanceObj { get; private set; }
+
+    public virtual void Init(My_GameInstance gameInstance)
+    {
+        //Init各类管理器
+        this.gameInstance = gameInstance;
+        this.gameInstanceObj = gameInstance.gameObject;
+
+        print("----this is init:" + this.gameInstance.GetType().Name);
+
+        My_ScenesMgr.Create().Init();
+    }
+
+    public void OnUpdate()
+    {
+        //Update各类管理器
+        print("----this is Update:" + this.gameInstance);
+
+        My_ScenesMgr.Instance.OnUpdate();
+    }
+
+    public void OnDestroy()
+    {
+        //Destory各类管理器
+        print("----this is Destroy:" + this.gameInstance);
+
+        My_ScenesMgr.Instance.OnDestory();
+    }
+
+    public Coroutine StartCoroutine(IEnumerator routine)
+    {
+        return this.gameInstance.StartCoroutine(routine);
+    }
+
+    public void Coroutine(IEnumerator routine)
+    {
+        this.gameInstance.StopCoroutine(routine);
+    }
+}
