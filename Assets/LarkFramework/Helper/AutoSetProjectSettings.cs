1ecc7baa032677171b866b9fd626ab7445e345fa
evesgf <evesgf@163.com>
2016-11-25 19:14:33 +0800

    新增项目配置自动化文件

---
+++ Assets/LarkFramework/Helper/AutoSetProjectSettings.cs
@@ -0,0 +1,49 @@
+﻿/*---------------------------------------------------------------
+ * 作者：evesgf    创建时间：2016-11-25 14:53:12
+ * 修改：evesgf    修改时间：2016-11-25 14:53:17
+ * 联系：Email     821113542@qq.com
+ *
+ * 版本：V0.0.1
+ * 
+ * 描述：自动配置Project Settings
+ ---------------------------------------------------------------*/
+
+#if UNITY_EDITOR
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+namespace LarkFramework
+{
+    public class AutoSetProjectSettings : MonoBehaviour
+    {
+        [MenuItem("LarkFramework/AutoSetProjectSettings")]
+        internal static void AutoSet()
+        {
+            if (EditorSettings.externalVersionControl != "Visible Meta Files")
+                EditorSettings.externalVersionControl = "Visible Meta Files";
+
+            if (EditorSettings.serializationMode != SerializationMode.ForceText)
+                EditorSettings.serializationMode = SerializationMode.ForceText;
+
+            if (PlayerSettings.displayResolutionDialog != ResolutionDialogSetting.HiddenByDefault)
+                PlayerSettings.displayResolutionDialog = ResolutionDialogSetting.HiddenByDefault;
+
+            if (PlayerSettings.showUnitySplashScreen)
+                PlayerSettings.showUnitySplashScreen = false;
+
+            if (PlayerSettings.apiCompatibilityLevel != ApiCompatibilityLevel.NET_2_0)
+                PlayerSettings.apiCompatibilityLevel = ApiCompatibilityLevel.NET_2_0;
+
+            if (PlayerSettings.defaultInterfaceOrientation != UIOrientation.LandscapeLeft)
+                PlayerSettings.defaultInterfaceOrientation = UIOrientation.LandscapeLeft;
+
+            if (PlayerSettings.bundleIdentifier.Equals("com.Company.ProductName"))
+                PlayerSettings.bundleIdentifier = "com.evesgf.product";
+
+            Debug.Log("Auto Set Project Settings Finish!");
+        }
+    }
+
+}
+#endif
